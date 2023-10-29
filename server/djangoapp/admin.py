4fdfd708484a2766c3a4d9c1abb9472ca4699e87
Zman82-dot <dorian.r.coleman@gmail.com>
2023-10-28 23:45:41 -0400

    Registered Admin Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -10,7 +10,15 @@ class CarModelInline(admin.StackedInline):
     extra = 2
 
 # CarModelAdmin class
-
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['car_make','name', 'dealer_id', 'model_type','year' ]
+    list_filter = ['model_type', 'car_make', 'dealer_id', 'year',]
+    search_fields = ['car_make', 'name']
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name']
+    inlines = [CarModelInline]
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
